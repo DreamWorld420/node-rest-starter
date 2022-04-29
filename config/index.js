25dcc7aff31131c9cba40576ac0fbad6013ffed9
DreamWorld420 <dreamworld420@protonmail.com>
2022-04-29 18:09:12 +0430

    feat: added config

---
+++ config/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config({ path: `${__dirname}/../.env` });
+
+module.exports = {
+	node_env: process.env.NODE_ENV,
+	port_dev: process.env.PORT_DEV,
+	host_dev: process.env.HOST_DEV,
+	mongodb_uri_dev: process.env.MONGODB_URI_DEV,
+};
